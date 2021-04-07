467ec3e3db152c359ac97ec0a45d62dbc2ecf215
AdamB <adam.boczula@s3connectedhealth.com>
2021-04-07 10:33:55 +0200

    Z grubsza skończone
    Refaktoring
    
    Material - ogarnij style i kolory
    
    Ogarnij czy wszystko działa dobrze

--- src/app/resolvers/recipe.resolver.ts
+++ src/app/resolvers/recipe.resolver.ts
@@ -3,7 +3,6 @@ import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { Recipe } from '@core/models';
 import { ofType, Actions } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
 import { first, map, tap } from 'rxjs/operators';
 import { RecipeActions, RecipeEditActions } from '../core/store/actions';
 import * as fromRecipe from '../core/store/reducers';
@@ -14,20 +13,29 @@ import * as fromRecipe from '../core/store/reducers';
 export class RecipeResolver implements Resolve<boolean> {
   constructor(private store: Store<fromRecipe.State>, private actions$: Actions) {}
 
-  public resolve(route: ActivatedRouteSnapshot): Observable<boolean> | boolean {
-    this.store.dispatch(RecipeActions.fetchRecipes());
-    this.actions$.pipe(
-      ofType(RecipeActions.fetchRecipesSuccess))
+  public resolve(route: ActivatedRouteSnapshot): boolean {
+    this.store.select(fromRecipe.selectEditedRecipe)
       .pipe(
         first(),
-        map(({ recipes }) => recipes.find((r: Recipe) => r.name.toLowerCase() === route.params.recipeName.toLowerCase())),
-        tap(editedRecipe => {
-            if (editedRecipe) {
-              this.store.dispatch(RecipeEditActions.editRecipe({ editedRecipe }));
-            }
+        map(er => {
+          if (!er) {
+            this.actions$.pipe(
+              ofType(RecipeActions.fetchRecipesSuccess))
+              .pipe(
+                first(),
+                map(({ recipes }) => recipes.find((r: Recipe) => r.name.toLowerCase() === route.params.recipeName.toLowerCase())),
+                tap(editedRecipe => {
+                    if (editedRecipe) {
+                      this.store.dispatch(RecipeEditActions.editRecipe({ editedRecipe }));
+                    }
+                  }
+                ),
+              ).subscribe();
+            this.store.dispatch(RecipeActions.fetchRecipes());
           }
-        )).subscribe();
 
+        })
+      ).subscribe();
     return true;
   }
 }
